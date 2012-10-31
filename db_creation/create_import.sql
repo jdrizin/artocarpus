91c6548be9a5b23b1df7cfd1cb1c31fb0afd5002
Josh Drizin <josh@respectablename.com>
2012-10-31 14:40:45 -0500

    add the database creation files

--- db_creation/create_import.sql
+++
@@ -1,3 +0,0 @@
-# our files have crlf line endings, so lines are termianted by \r\n, not just \n
-load data local infile '/home/josh/csvs/geneticproject.txt' into table geneticproject fields terminated by ',' enclosed by '"' lines terminated by '\r\n';
-
