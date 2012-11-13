833b6b9a3ed1a325cb6b759bc8a2ddcdbe48a760
Josh Drizin <josh@respectablename.com>
2012-11-13 22:02:27 +0400

    add geospatial to the foreign keys

--- db_creation/02_foreign_keys.sql
+++ db_creation/02_foreign_keys.sql
@@ -11,14 +11,14 @@ ADD PRIMARY KEY (ID);
 
 #make it increment
 ALTER TABLE collectiondata
-MODIFY COLUMN ID INT(5) AUTO_INCREMENT;
+MODIFY COLUMN ID INT(6) AUTO_INCREMENT;
 
 #########################################################################################
-# define and add foreign keys, but don't add data yet
+# add new foreign keys 
 
 #add the foreign key to the geneticproject table
 ALTER TABLE geneticproject
-ADD fk_genetic_collection_ID INT(5);
+ADD fk_genetic_collection_ID INT(6);
 
 UPDATE geneticproject
 SET fk_genetic_collection_ID=GeneticID;
@@ -29,7 +29,7 @@ REFERENCES collectiondata(ID);
 
 #add the foreign key to annotations
 ALTER TABLE annotations
-ADD fk_annotations_collection_ID INT(5);
+ADD fk_annotations_collection_ID INT(6);
 
 UPDATE annotations
 SET fk_annotations_collection_ID=AnnotationID;
@@ -38,7 +38,16 @@ ALTER TABLE annotations
 ADD FOREIGN KEY (fk_annotations_collection_ID)
 REFERENCES collectiondata(ID);
 
+#add the foreign key to geospatial 
+ALTER TABLE geospatial 
+ADD fk_geospatial_collection_ID INT(6);
+
+UPDATE geospatial
+SET fk_geospatial_collection_ID=geoID;
+
+ALTER TABLE geospatial
+ADD FOREIGN KEY (fk_geospatial_collection_ID)
+REFERENCES collectiondata(ID);
+
 #don't add colby's data yet, as I don't think her IDs match the main database
-#########################################################################################
-#now fill out the foreign key data for annotations and geneticproject
 
