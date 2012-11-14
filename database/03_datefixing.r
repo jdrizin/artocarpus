dc9f724b1e67926e457e5385f4bc50baa3b99d4c
Josh Drizin <josh@respectablename.com>
2012-11-14 12:25:20 -0600

    r script to help fix the bad date data. initial commit with a comment

---
+++ database/03_datefixing.r
@@ -0,0 +1,4 @@
+#this script is an attempt to bust apart some real ugly date data. the idea
+# is to break apart the single date field along any punctuation and write
+# those into three other values, then I'll have to flip through by hand
+# to solve the rest :/
