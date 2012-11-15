715fd56bcee239b30ac071e525394230688dc1cf
Josh Drizin <josh@respectablename.com>
2012-11-15 08:44:42 -0600

    finish up the script. this busts apart the date fields, sorta. it should make fixing everything go faster. i'm sorry for abusing R.

--- database/03_datefixing.r
+++ database/03_datefixing.r
@@ -12,21 +12,26 @@ colldate <- as.character(baddate$CollectionDate)
 colldate <- gsub("[\\.,-/]", ".", colldate)
 datelist <- strsplit(colldate,"\\.") #it's a regexp. gotta escape the .
 
-#begin an ugly loop. apply might be better, but i know how to do it this way.
-#for(i in 1:length(datelist)){
+#create a dummy database
+dumvec <- rep("zero", 1245)
+dumdf  <- data.frame(one=dumvec, two=dumvec, three=dumvec, 
+							stringsAsFactors=FALSE)
+
+#begin an ugly loop. 
 i<-1
 while(i <= length(datelist)){
 	tempvec <- unlist(datelist[i])
 	if(length(tempvec) < 3){
 		tempvec <- c(tempvec, rep(0, (3-length(tempvec))))
-		}
+	}
 	#now tempvec is 3 items long. time to dump it into a new dataframe
-	if(i == 1){
-		datedf.new <- data.frame(one=tempvec[1], two=tempvec[2], three=tempvec[3])	
-	}
-	else{
-		tempdf <- data.frame(one=tempvec[1], two=tempvec[2], three=tempvec[3])
-		datedf.new <- rbind(datedf.new, tempvec)
-	}
+	dumdf[i,1] <- as.character(tempvec[1])
+	dumdf[i,2] <- as.character(tempvec[2])
+	dumdf[i,3] <- as.character(tempvec[3])
+	print(i)
 	i <- i+1
 }
+
+#merge the dfs and dump the dataframe
+bigdate <- cbind(baddate, dumdf)
+write.csv(bigdate, file="private/bigdate.csv")
