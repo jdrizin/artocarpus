234aacfbc0ead1a837c2828043844aacfc566207
Josh Drizin <josh@respectablename.com>
2012-11-17 01:19:53 +0400

    insert the data and add the foreign keys, so the new annotation table should be all set

--- database/03_annotationfixing.sql
+++ database/03_annotationfixing.sql
@@ -12,3 +12,27 @@ SELECT *
 #cat annotationfix.csv | sed 's/\ /\",\"/2' > annotationsplit.csv
 #tac annotationsplit.csv|sed 's/\ /\",\"/1' |tac> annotationdone.csv
 
+#drop the old table
+DROP TABLE annotations;
+#create a new table for this data
+CREATE TABLE annotations (annotationID INT(6), Genus VARCHAR(50),
+								Species VARCHAR(50), Annotation VARCHAR(5000), AnnoDate
+								VARCHAR(25))
+	ENGINE=Innodb;
+
+#insert the new data
+LOAD DATA LOCAL INFILE '/home/josh/csvs/annotationdone.csv'
+	into table annotations fields terminated by ','
+	enclosed by '"' lines terminated by '\n';
+
+#set the foreign key
+#add the foreign key to annotations
+ALTER TABLE annotations
+ADD fk_annotations_collection_ID INT(6);
+
+UPDATE annotations
+SET fk_annotations_collection_ID=AnnotationID;
+
+ALTER TABLE annotations
+ADD FOREIGN KEY (fk_annotations_collection_ID)
+REFERENCES collectiondata(ID);
