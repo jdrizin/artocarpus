b2fd190d80c8409037f219d616e32dd33bd7c38f
Josh Drizin <josh@respectablename.com>
2012-11-27 01:43:08 +0400

    output data for nyree to fix

--- database/00_niftyqueries.sql
+++ database/00_niftyqueries.sql
@@ -10,4 +10,47 @@ select ID,MostRecentGenus,Genus FROM collectiondata
 	WHERE MostRecentGenus <> Genus;
 
 select ID,MostRecentSpecies,Species FROM collectiondata 
-	WHERE MostRecentSpecies <> Species; 
+	WHERE MostRecentSpecies <> Species;
+
+#dump out some data into a CSV with unix line endings.
+SELECT ID,Image_location 
+        INTO OUTFILE '/home/josh/csvs/imagelocationfix.csv'
+        FIELDS TERMINATED BY ','
+        ENCLOSED BY '"'
+        ESCAPED BY '\\'
+        LINES TERMINATED BY '\n'
+        FROM collectiondata;
+
+#dump out data for nyree to fix
+#dump genus/species errata
+
+select ID,MostRecentGenus,Genus 
+	FROM collectiondata 
+   WHERE MostRecentGenus <> Genus
+   INTO OUTFILE '/home/josh/csvs/genusmismatch.csv'
+   FIELDS TERMINATED BY ',' 
+   ENCLOSED BY '"' 
+   ESCAPED BY '\\'
+   LINES TERMINATED BY '\n'
+   ;
+
+select ID,MostRecentSpecies,Species 
+   FROM collectiondata    
+   WHERE MostRecentSpecies <> Species
+   INTO OUTFILE '/home/josh/csvs/speciesmismatch.csv'
+   FIELDS TERMINATED BY ','     
+   ENCLOSED BY '"'     
+   ESCAPED BY '\\'
+   LINES TERMINATED BY '\n'
+   ;
+
+select ID,MostRecentGenus,MostRecentSpecies,MostRecentAltID
+   FROM collectiondata
+   WHERE MostRecentAltID <> ''
+   INTO OUTFILE '/home/josh/csvs/altIDmismatch.csv'
+   FIELDS TERMINATED BY ','
+   ENCLOSED BY '"'
+   ESCAPED BY '\\'
+   LINES TERMINATED BY '\n'
+   ;
+
