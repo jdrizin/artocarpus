32542a12138ea1eb1f814e751bb554ebefda11bd
Josh Drizin <josh@respectablename.com>
2012-11-27 21:37:16 +0400

    build the database correctly, insert old data, add columns for later use

--- database/03_herbariumcleanup.sql
+++ database/03_herbariumcleanup.sql
@@ -12,6 +12,29 @@ CREATE TABLE herbarium (herbID INT(6),
    CollectionDateGood VARCHAR(15), VoucherLocations VARCHAR(500),
    SheetScanNumber VARCHAR(50), AccessionNumber VARCHAR(500),
    PlantDescription VARCHAR(5000), Reproductive VARCHAR(500),
-   Type VARCHAR(500), LocationInfo VARCHAR(5000), Habitat VARCHAR(5000),
-   OnLoan VARCHAR(10), DateLoanedOut VARCHAR(15), DateReturned VARCHAR(15))
-   ENGINE=Innodb; 
+   Type VARCHAR(500), LocationInfo VARCHAR(5000), Habitat VARCHAR(5000))
+   ENGINE=Innodb;
+
+#now we need a select query to fill in all of this data
+INSERT INTO herbarium
+	SELECT ID, Genus, Species, SpeciesAuthority, SubspVar, SubspeciesAuthority, 
+	DeterminedBy, HerbariumCollection, Collector, CollectionNumber, Vernacular,
+	CollectionDate, CollectionDateGood, VoucherLocations, SheetScanNumber,
+	AcessionNumber, PlantDescription, Reproductive, Type, LocationInfo, 
+	Habitat
+	FROM collectiondata;
+
+#now add the new columns
+ALTER TABLE herbarium
+   ADD OnLoan VARCHAR(10), 
+   ADD DateLoanedOut VARCHAR(15),
+   ADD DateReturned VARCHAR(15),
+   ADD fk_herbarium_collection_ID INT(6);
+
+#update the foreign key data
+UPDATE herbarium
+	set fk_herbarium_collection_ID = herbID;
+
+ALTER TABLE herbarium
+	ADD FOREIGN KEY (fk_herbarium_collection_ID)
+	REFERENCES collectiondata(ID);
