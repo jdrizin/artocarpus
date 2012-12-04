efcbb62a705dcd7776a65ad7798135e69ddd9ff9
Josh Drizin <josh@respectablename.com>
2012-12-04 08:22:21 -0800

    update geneseq table to better reflect real use

--- database/04_geneseq_test.sql
+++ database/04_geneseq_test.sql
@@ -1,11 +1,11 @@
 #create a test database for FASTA manipulation
 
-CREATE TABLE genesequences (geneseqID INT(6), SampleType VARCHAR(500),
+CREATE TABLE genesequences (geneseqID INT(6) AUTO_INCREMENT NOT NULL, SampleType VARCHAR(500),
         Extractor VARCHAR(500),ExtractionCode VARCHAR (500),
         ExtractionDate VARCHAR(500),ExtractionMethod VARCHAR(500),
-        locusUsed VARCHAR(500), sequence VARCHAR(10000),
-        quality VARCHAR(10000)
+        locusUsed VARCHAR(500), header VARCHAR(150), sequence VARCHAR(10000),
+        PRIMARY KEY(geneseqID)
         )
-        ENGINE=Innodb; 
 
+        ENGINE=Innodb; 
 
