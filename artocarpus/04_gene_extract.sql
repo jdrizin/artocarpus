08c25dcb2fa682c16ae5937b3316cf1c786f07f6
Josh Drizin <josh@respectablename.com>
2013-01-07 15:08:14 -0600

    partial code: might end up scrapped for reorg
    this is just saving what code's written: i may end up not dealing with
    this because we may totally reorganize the database, so for now, don't
    move geneticproject into extractions tables

--- artocarpus/04_gene_extract.sql
+++ artocarpus/04_gene_extract.sql
@@ -7,3 +7,13 @@ TRUNCATE extractions;
 ALTER TABLE extractions
     ADD COLUMN leafTakenDNA VARCHAR(50),
     ADD COLUMN extractionCode VARCHAR(200) AFTER extractionDate;
+
+#start shifting data
+UPDATE extractions as ex, geneticproject as gp
+    SET ex =gp.geneticID,
+    SET =gp.SampleTypeExtractedFrom,
+    SET ex.leafTakenDNA=gp.LeafTakenDNA,
+    SET ex.ExtractorName=gp.Extractor,
+    SET ex.extractionCode=gp.Extractioncode,
+    SET ex. =gp.ExtractionDate
+    
