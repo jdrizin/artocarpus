10af6842143fc3103549e8014116fdfcf76eb365
Josh Drizin <josh@respectablename.com>
2013-02-04 15:16:57 -0600

    start the script to reorganize microsat data
    include first part of script with notes and a small bit of the microsat data for working on the code.

---
+++ userscripts/microsat_reformat.R
@@ -0,0 +1,4 @@
+#this script will convert the across-column microsat loc-* to a format more
+#useful for our database: (taxon,id,ploidy) (id, locname, locdata), linked by id
+
+bfmicro <- read.csv("userscripts/breadfruit_microsat_testingdata.csv")
