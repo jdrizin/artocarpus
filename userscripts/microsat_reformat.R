9d9ac246cefc9c2641b906966f6c68cfd3af38ce
Josh Drizin <josh@respectablename.com>
2013-02-04 15:30:33 -0600

    pull out the locs: commit @ end of day

--- userscripts/microsat_reformat.R
+++ userscripts/microsat_reformat.R
@@ -2,3 +2,5 @@
 #useful for our database: (taxon,id,ploidy) (id, locname, locdata), linked by id
 
 bfmicro <- read.csv("userscripts/breadfruit_microsat_testingdata.csv")
+
+grep("loc", names(bfmicro), value=T) #pull out only the loc*
